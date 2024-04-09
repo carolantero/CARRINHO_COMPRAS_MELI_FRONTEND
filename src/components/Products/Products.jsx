0f21b4c3ab5d85425ef2cfd15ad7391ad54ecf3a
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-09 00:40:20 -0300

    feat: utilizando o componente GradientCircularProgress no componente Products para mostrar carregamento

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,26 +1,36 @@
-import React, {useState, useEffect} from "react"
-import './Products.css'
-import ProductCard from "./ProductCard"
-import getAllProducts from "../../api/apiShoppingCart"
+import React, { useState, useEffect } from "react";
+import "./Products.css";
+import ProductCard from "./ProductCard";
+import getAllProducts from "../../api/apiShoppingCart";
+import GradientCircularProgress from "../Loading/Loading";
 
 function Products() {
-
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getAllProducts().then((result) => {
-      setProducts(result)
-    })
+      setProducts(result);
+    });
   }, []);
 
+  useEffect(() => {
+    if (products.length > 0) {
+      setLoading(false);
+    }
+  }, [products]);
 
-  return (
-    <div className="products">
-      {
-      products.map((product) => <ProductCard key={product.product_id} data={product}/>)
-      }
+  return loading ? (
+    <div className="products-loading">
+      <GradientCircularProgress />
     </div>
-  )
+  ) : (
+    <div className="products">
+      {products.map((product) => (
+        <ProductCard key={product.product_id} data={product} />
+      ))}
+    </div>
+  );
 }
-  
-  export default Products
+
+export default Products;
