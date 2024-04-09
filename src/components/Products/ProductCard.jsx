90e2086701bbd8e3e359256eb2a3e5c81745b50f
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-09 00:41:40 -0300

    fix: ajustando identação do componente ProductCard

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,25 +1,26 @@
-import React from "react"
-import './ProductCard.css'
+import React from "react";
+import "./ProductCard.css";
 
 function ProductCard({ data }) {
+  const { product_id, product_title, product_image, product_price } = data;
 
-  const {product_id, product_title, product_image, product_price} = data;
-
-    return (
-      <div className="product-card" key={product_id}>
-       <img src={product_image} alt="product" className="card-image"/>
-       <div className="card-infos">
+  return (
+    <div className="product-card" key={product_id}>
+      <img src={product_image} alt="product" className="card-image" />
+      <div className="card-infos">
         <h2 className="card-title">{product_title}</h2>
-        <h2 className="card-price">{product_price.toLocaleString('pt-br', {
-          style: 'currency',
-          currency: 'BRL'
-        })}</h2>
-       </div>
-       <button type="button" className="button-add-card">
-        <span>Adicionar ao carrinho</span> 
-       </button>
+        <h2 className="card-price">
+          {product_price.toLocaleString("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </h2>
       </div>
-    )
-  }
-  
-  export default ProductCard
+      <button type="button" className="button-add-card">
+        <span>Adicionar ao carrinho</span>
+      </button>
+    </div>
+  );
+}
+
+export default ProductCard;
