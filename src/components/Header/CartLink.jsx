de7fe4545adf2b51a0d2f05e593877e80863f8bf
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 20:18:02 -0300

    feat: add função handle no onClick para chamar os produtos adicionados no carrinho

--- src/components/Header/CartLink.jsx
+++ src/components/Header/CartLink.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BsCart2 } from "react-icons/bs";
+import { getCartProducts } from "../../api/apiShoppingCart";
 import "./CartLink.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { GET_CART_PRODUCTS } from "../../actions/cartActions";
 
 function CartLink() {
+  const dispatch = useDispatch();
+  const addCartProducts = useSelector((state) => state.cart.addCartProducts);
+
+  const handleGetCartProducts = () => {
+    getCartProducts().then((result) => {
+      dispatch({ type: GET_CART_PRODUCTS, payload: result });
+    });
+  };
+
   return (
     <div className="cart-link">
-      <Link to="/cart">
+      <Link to="/cart" onClick={handleGetCartProducts}>
         <BsCart2 className="cart-link-item" />
         <span className="cart-status">0</span>
       </Link>
