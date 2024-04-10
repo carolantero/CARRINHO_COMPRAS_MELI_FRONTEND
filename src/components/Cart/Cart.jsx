4ba20e3d660a5d62b9e1e3ae72e2e9ce98a185a6
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 02:20:38 -0300

    feat: consumindo os dados armazenados no meu reducer, em outro componente

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Cart.css";
 import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
 
 function Cart() {
+  const allProducts = useSelector((state) => state.cart.allProducts);
+
   return (
     <div className="cart">
       <div className="cart-items">
