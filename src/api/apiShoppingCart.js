bef18fded2556d8297ecbcbb043cf9266346047b
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 20:15:14 -0300

    feat: add rotas POST e GET para controlar fluxo dos produtos no carrinho

--- src/api/apiShoppingCart.js
+++ src/api/apiShoppingCart.js
@@ -1,8 +1,28 @@
-const getAllProducts = async (category) => {
-    const response = await fetch(`http://127.0.0.1:8000/get_all_products`);
-    const data = await response.json();
+export const getAllProducts = async (category) => {
+  const response = await fetch(`http://127.0.0.1:8000/get_all_products`);
+  const data = await response.json();
 
-    return data;
+  return data;
 };
 
-export default getAllProducts;
+export const postCartProducts = async (item) => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/post_cart_products/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+    const data = await response.json();
+  } catch (error) {
+    console.error("Error adding item to cart:", error);
+  }
+};
+
+export const getCartProducts = async () => {
+  const response = await fetch(`http://127.0.0.1:8000/get_cart_products`);
+  const data = await response.json();
+
+  return data;
+};
