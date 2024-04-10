91dea47e6be3bcdde1baa74e6d2e98a75e577fd9
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 15:00:21 -0300

    feat: colocando dinamica de dados dinamicos no componente ProductCard

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ProductCard.css";
+import { useDispatch } from "react-redux";
+import { CART_PRODUCTS } from "../../actions/cartActions";
+import { useSelector } from "react-redux";
 
-function ProductCard({ data }) {
-  const { product_id, product_title, product_image, product_price } = data;
+function ProductCard({ product }) {
+  const dispatch = useDispatch();
+  const { product_id, product_title, product_image, product_price } = product;
+  const allCartProducts = useSelector((state) => state.cart.allCartProducts);
+
+  const handleAddCar = () => {
+    dispatch({ type: CART_PRODUCTS, payload: [...allCartProducts, product] });
+  };
 
   return (
-    <div className="product-card" key={product_id}>
-      <img src={product_image} alt="product" className="card-image" />
+    <div className="product-card">
+      <img src={product_image} alt="product image" className="card-image" />
       <div className="card-infos">
         <h2 className="card-title">{product_title}</h2>
         <h2 className="card-price">
@@ -16,7 +25,7 @@ function ProductCard({ data }) {
           })}
         </h2>
       </div>
-      <button type="button" className="button-add-card">
+      <button type="button" className="button-add-card" onClick={handleAddCar}>
         <span>Adicionar ao carrinho</span>
       </button>
     </div>
