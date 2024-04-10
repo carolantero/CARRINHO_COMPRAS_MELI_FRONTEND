c11974fd163bfcb2e2692150c3e6b97202412164
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 15:02:37 -0300

    feat: componente CartItem consumindo dados dinamicos

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./CartItem.css";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { useSelector } from "react-redux";
 
-function CartItem({ data }) {
+function CartItem({ product }) {
+  const { product_id, product_title, product_image, product_price } = product;
+
   return (
     <div className="cart-item">
       <div className="cart-item-content">
-        <img src="" alt="imagem do produto" className="cart-item-image" />
+        <img
+          src={product_image}
+          alt="product image"
+          className="cart-item-image"
+        />
         <div>
-          <h3 className="cart-item-title">Titulo</h3>
-          <h3 className="cart-item-price">310</h3>
+          <h3 className="cart-item-title">{product_title}</h3>
+          <h3 className="cart-item-price">
+            {product_price.toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </h3>
         </div>
       </div>
       <button className="button-item-remove" type="button">
