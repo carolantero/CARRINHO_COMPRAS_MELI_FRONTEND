5f26ac911cfd12653e440069554f99b363dc9a04
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 12:06:23 -0300

    feat: passando componentes de rotas das paginas ajustados ao componente Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import CartButton from "../CartButton/CartButton";
+import CartLink from "./CartLink";
+import HomeLink from "./HomeLink";
 import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
       <div className="header-container">
-        <Link to="/">HOME</Link>
-        <CartButton />
+        <HomeLink />
+        <CartLink />
       </div>
     </header>
   );
