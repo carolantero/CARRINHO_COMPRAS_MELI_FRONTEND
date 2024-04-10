e48f5826b6bebb067b7271299c4b17c7ea23d7a0
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 02:19:46 -0300

    feat: despachando uma ação com o retorno da minha requisição

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,10 +3,16 @@ import "./Products.css";
 import ProductCard from "./ProductCard";
 import getAllProducts from "../../api/apiShoppingCart";
 import GradientCircularProgress from "../Loading/Loading";
+import { useDispatch } from "react-redux";
+import { GET_ALL_PRODUCTS } from "../../actions/cartActions";
+import { useSelector } from "react-redux";
 
 function Products() {
+  const dispatch = useDispatch();
+
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const allProducts = useSelector((state) => state.cart.allProducts);
 
   useEffect(() => {
     getAllProducts().then((result) => {
@@ -17,6 +23,7 @@ function Products() {
   useEffect(() => {
     if (products.length > 0) {
       setLoading(false);
+      dispatch({ type: GET_ALL_PRODUCTS, payload: products });
     }
   }, [products]);
 
