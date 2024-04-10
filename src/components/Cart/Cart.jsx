5285ee5527bb75d0531c27b15570dd599b33132a
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 15:01:31 -0300

    feat: atribuindo dados dinamicos ao componente CartItem

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Cart.css";
 import CartItem from "./CartItem";
+import GradientCircularProgress from "../Loading/Loading";
 import { useSelector } from "react-redux";
 
 function Cart() {
-  const allProducts = useSelector((state) => state.cart.allProducts);
+  const [loading, setLoading] = useState(true);
+  const allCartProducts = useSelector((state) => state.cart.allCartProducts);
 
-  return (
+  useEffect(() => {
+    if (allCartProducts.length > 0) {
+      setLoading(false);
+    }
+  }, [allCartProducts]);
+
+  return loading ? (
     <div className="cart">
       <div className="cart-items">
         <h2>Carrinho</h2>
-        <CartItem />
+        <GradientCircularProgress />
+      </div>
+      <div className="cart-resume">valor total</div>
+    </div>
+  ) : (
+    <div className="cart">
+      <div className="cart-items">
+        <h2>Carrinho</h2>
+        {allCartProducts.map((product) => (
+          <CartItem key={product.product_id} product={product} />
+        ))}
       </div>
       <div className="cart-resume">valor total</div>
     </div>
