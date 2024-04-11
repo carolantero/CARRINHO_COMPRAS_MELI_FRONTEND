37f73be14e2cca4d331567dc625c912092ec2fa9
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-11 14:02:26 -0300

    fix: tirando função handle do arquivo atual e importando da pasta utils

--- src/components/Header/CartLink.jsx
+++ src/components/Header/CartLink.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BsCart2 } from "react-icons/bs";
-import { getCartProducts } from "../../api/apiShoppingCart";
 import "./CartLink.css";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { GET_CART_PRODUCTS } from "../../actions/cartActions";
+import { handleGetCartProducts } from "../../utils/handleGetCartProducts";
 
 function CartLink() {
   const dispatch = useDispatch();
   const addCartProducts = useSelector((state) => state.cart.addCartProducts);
 
-  const handleGetCartProducts = () => {
-    getCartProducts().then((result) => {
-      dispatch({ type: GET_CART_PRODUCTS, payload: result });
-    });
-  };
-
   return (
     <div className="cart-link">
-      <Link to="/cart" onClick={handleGetCartProducts}>
+      <Link to="/cart" onClick={handleGetCartProducts(dispatch)}>
         <BsCart2 className="cart-link-item" />
         {addCartProducts.length > 0 && (
           <span className="cart-status">{addCartProducts.length}</span>
