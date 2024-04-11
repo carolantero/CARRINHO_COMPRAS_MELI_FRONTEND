561c08acf20384fe99084905a786be2f8c8444ad
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-10 23:55:09 -0300

    feat: usando os estados da camada service para gerir minhas rotas e fazer o fluxo de colocar e tirar os produtos do carrinho

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./CartItem.css";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { useSelector } from "react-redux";
+import { postCartProducts } from "../../api/apiShoppingCart";
+import { useDispatch } from "react-redux";
+import { getCartProducts } from "../../api/apiShoppingCart";
+import {
+  POST_CART_PRODUCTS,
+  GET_CART_PRODUCTS,
+  ADD_CART_PRODUCTS,
+} from "../../actions/cartActions";
 
 function CartItem({ product }) {
+  const dispatch = useDispatch();
   const { product_id, product_title, product_image, product_price } = product;
+  const getCartProductsItems = useSelector(
+    (state) => state.cart.getCartProducts
+  );
+  const postCartProductsItem = useSelector(
+    (state) => state.cart.postCartProducts
+  );
+
+  const handleGetCartProducts = () => {
+    getCartProducts().then((result) => {
+      dispatch({ type: GET_CART_PRODUCTS, payload: result });
+    });
+  };
+
+  const handleDeleteCartProduct = async () => {
+    const updatedItems = getCartProductsItems.filter(
+      (item) => item.product_id != product_id
+    );
+
+    dispatch({ type: ADD_CART_PRODUCTS, payload: updatedItems });
+
+    const updatedCart = await postCartProducts(updatedItems);
+    dispatch({
+      type: POST_CART_PRODUCTS,
+      payload: updatedCart,
+    });
+  };
+
+  useEffect(() => {
+    if (postCartProductsItem.length > 0) {
+      handleGetCartProducts();
+    }
+  }, [postCartProductsItem]);
 
   return (
     <div className="cart-item">
@@ -24,7 +65,12 @@ function CartItem({ product }) {
           </h3>
         </div>
       </div>
-      <button className="button-item-remove" type="button">
+
+      <button
+        className="button-item-remove"
+        type="button"
+        onClick={() => handleDeleteCartProduct()}
+      >
         <RiDeleteBin6Line />
       </button>
     </div>
